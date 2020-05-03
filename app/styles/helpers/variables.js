5ead09d34ab462a5a7d2bbc5db56132c18c53ec0
ali aziz <ali@payzilch.com>
2020-05-03 16:53:51 +0100

    inint whatIsIt component

--- app/styles/helpers/variables.js
+++ app/styles/helpers/variables.js
@@ -1,32 +1,18 @@
 // Default States
-export const primary = '#32d282';
-export const secondary = '#32d282';
-export const accent = '#3CE66D';
+export const primary = '#481380';
+export const secondary = '#7f78d2';
+export const accent = '#efb1ff';
 export const success = '#46BB4A';
 export const danger = '#FF0000';
 export const warning = '#F5A623';
 export const lightGrey = '#D8D8D8';
 export const grey = '#9B9B9B';
 export const darkGrey = '#4A4A4A';
-export const blue = '#00baef';
-export const gentleBlue = '#E6ECF5';
-export const iconBlue = '#99A1AD';
 export const white = '#FFFFFF';
 export const black = '#000000';
-export const bodyBg = '#f5f5f5';
+export const bodyBg = '#ffe2ff';
 export const validationRed = '#ff3e00';
 export const link = '#0000EE';
-export const shadow = 'rgba(216, 216, 216, 0.43)';
-export const activeBorder = '#32d282';
-export const searchBorder = '#b1adad';
-export const groupBg = '#F2F2F2';
-export const greyBorder = '#EFEFEF';
-export const greyHover = '#F6F6F6';
-export const crimson = '#D91F51';
-export const lockBg = '#AA0202';
-export const plusBorder = '#EBEBEB';
-export const zebraRowBg = '#FAF9FF';
-export const zebraBg = '#FAF9FF';
 
 // Hovered States
 export const primaryHover = '#32d282';
