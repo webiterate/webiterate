a8bcb7ae96fb9d7071f33c7500658428586b1e70
ali aziz <ali@payzilch.com>
2020-05-24 16:57:38 +0100

    init paid HOC

--- app/containers/ExperimentsPage/components/experimentsList/components/mojiPayBrief.js
+++ app/containers/ExperimentsPage/components/experimentsList/components/mojiPayBrief.js
@@ -7,20 +7,31 @@
 import React, { memo } from 'react';
 import { Link } from 'react-router-dom';
 import { compose } from 'redux';
+import PropTypes from 'prop-types';
+import paidContent from 'components/PaidContent';
 // import { FormattedMessage } from 'react-intl';
 import { ExperimentBriefWrapper } from '../experiments-styles';
 // import messages from '../messages';
 
-export function MojiPayBrief() {
+export function MojiPayBrief(props) {
   return (
     <ExperimentBriefWrapper>
       <div>
-        <h1>Moji Pay Experiment</h1>
-        <p>The description will go here</p>
+        {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions,jsx-a11y/click-events-have-key-events */}
+        <h1 onClick={props.togglePaid}>Moji Pay Experiment</h1>
+        {props.paid && <p>The description will go here</p>}
       </div>
       <Link to="/experiment/moji_pay">Check it out</Link>
     </ExperimentBriefWrapper>
   );
 }
 
-export default compose(memo)(MojiPayBrief);
+MojiPayBrief.propTypes = {
+  togglePaid: PropTypes.bool,
+  paid: PropTypes.bool,
+};
+
+export default compose(
+  memo,
+  paidContent,
+)(MojiPayBrief);
