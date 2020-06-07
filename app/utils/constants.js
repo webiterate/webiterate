91f3d6ff483f39e1effcbb17361be6f6de202193
wndaiga <w.ndaiga@gmail.com>
2020-06-07 14:42:27 +0100

    initial frontend interledger

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,3 +1,17 @@
 export const RESTART_ON_REMOUNT = '@@saga-injector/restart-on-remount';
 export const DAEMON = '@@saga-injector/daemon';
 export const ONCE_TILL_UNMOUNT = '@@saga-injector/once-till-unmount';
+export const INTERLEDGER = {
+  XPRING: {
+    BASE_URL: window.location.origin,
+    ENDPOINTS: {
+      BALANCE: '/payment/balance',
+    },
+    OPT: {
+      header: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    },
+  },
+};
